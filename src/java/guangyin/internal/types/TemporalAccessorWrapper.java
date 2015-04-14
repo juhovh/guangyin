0336dbba661a6d1035c06bdc20de46bcee5f3876
Juho Vähä-Herttua <juhovh@iki.fi>
2015-04-14 17:19:48 +0300

    Create an actual map of TemporalAccessor in wrapper.

--- src/java/guangyin/internal/types/TemporalAccessorWrapper.java
+++ src/java/guangyin/internal/types/TemporalAccessorWrapper.java
@@ -2,30 +2,39 @@ package guangyin.internal.types;
 
 import clojure.lang.Keyword;
 import clojure.lang.ILookup;
+import clojure.lang.IMapEntry;
 import clojure.lang.IPersistentMap;
+import clojure.lang.PersistentArrayMap;
 import java.time.temporal.TemporalField;
 import java.time.temporal.TemporalAccessor;
 
 public class TemporalAccessorWrapper extends ObjectWrapper implements ILookup {
-    protected IPersistentMap keymap;
     private TemporalAccessor wrapped;
+    protected IPersistentMap keymap;
+    protected IPersistentMap valmap;
 
     public TemporalAccessorWrapper(IPersistentMap keymap, TemporalAccessor wrapped) {
         super(wrapped);
-        this.keymap = keymap;
+
         this.wrapped = wrapped;
+        this.keymap = keymap;
+
+        IPersistentMap valmap = PersistentArrayMap.EMPTY;
+        for (Object val : keymap) {
+            IMapEntry entry = (IMapEntry) val;
+            TemporalField field = (TemporalField) entry.val();
+            if (wrapped.isSupported(field)) {
+                valmap = valmap.assoc(entry.key(), wrapped.getLong(field));
+            }
+        }
+        this.valmap = valmap;
     }
 
     public Object valAt(Object key) {
-        return this.valAt(key, null);
+        return this.valmap.valAt(key);
     }
 
     public Object valAt(Object key, Object notFound) {
-        Keyword keyword = (Keyword) key;
-        TemporalField field = (TemporalField) keyword.invoke(this.keymap);
-        if (field == null || !wrapped.isSupported(field)) {
-            return notFound;
-        }
-        return wrapped.getLong(field);
+        return this.valmap.valAt(key, notFound);
     }
 }
