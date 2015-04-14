c3f2aeb550e0f0cf416fb9e5063d5af18ae5173b
Juho Vähä-Herttua <juhovh@iki.fi>
2015-04-14 18:35:51 +0300

    Cleanup wrappers based on valmap change earlier.
    Also makes valmap lazily evaluated and implements more interface in
    TemporalAccessorWrapper.

--- src/java/guangyin/internal/types/TemporalAccessorWrapper.java
+++ src/java/guangyin/internal/types/TemporalAccessorWrapper.java
@@ -1,40 +1,62 @@
 package guangyin.internal.types;
 
+import java.util.Iterator;
+import clojure.lang.ISeq;
 import clojure.lang.Keyword;
+import clojure.lang.Seqable;
+import clojure.lang.Counted;
 import clojure.lang.ILookup;
 import clojure.lang.IMapEntry;
 import clojure.lang.IPersistentMap;
-import clojure.lang.PersistentArrayMap;
+import clojure.lang.PersistentHashMap;
 import java.time.temporal.TemporalField;
 import java.time.temporal.TemporalAccessor;
 
-public class TemporalAccessorWrapper extends ObjectWrapper implements ILookup {
-    private TemporalAccessor wrapped;
+public class TemporalAccessorWrapper extends ObjectWrapper
+  implements Iterable, Seqable, Counted, ILookup {
     protected IPersistentMap keymap;
-    protected IPersistentMap valmap;
+    private TemporalAccessor wrapped;
+    private IPersistentMap valmap;
 
-    public TemporalAccessorWrapper(IPersistentMap keymap, TemporalAccessor wrapped) {
+    public TemporalAccessorWrapper(IPersistentMap keymap,
+                                   TemporalAccessor wrapped) {
         super(wrapped);
-
-        this.wrapped = wrapped;
         this.keymap = keymap;
+        this.wrapped = wrapped;
+    }
 
-        IPersistentMap valmap = PersistentArrayMap.EMPTY;
-        for (Object val : keymap) {
-            IMapEntry entry = (IMapEntry) val;
-            TemporalField field = (TemporalField) entry.val();
-            if (wrapped.isSupported(field)) {
-                valmap = valmap.assoc(entry.key(), wrapped.getLong(field));
+    protected IPersistentMap valmap() {
+        if (this.valmap == null) {
+            IPersistentMap valmap = PersistentHashMap.EMPTY;
+            for (Object val : keymap) {
+                IMapEntry entry = (IMapEntry) val;
+                TemporalField field = (TemporalField) entry.val();
+                if (wrapped.isSupported(field)) {
+                    valmap = valmap.assoc(entry.key(), wrapped.getLong(field));
+                }
             }
+            this.valmap = valmap;
         }
-        this.valmap = valmap;
+        return this.valmap;
+    }
+
+    public Iterator iterator() {
+        return this.valmap().iterator();
+    }
+
+    public ISeq seq() {
+        return this.valmap().seq();
+    }
+
+    public int count() {
+        return this.valmap().count();
     }
 
     public Object valAt(Object key) {
-        return this.valmap.valAt(key);
+        return this.valmap().valAt(key);
     }
 
     public Object valAt(Object key, Object notFound) {
-        return this.valmap.valAt(key, notFound);
+        return this.valmap().valAt(key, notFound);
     }
 }
--- src/java/guangyin/internal/types/TemporalWrapper.java
+++ src/java/guangyin/internal/types/TemporalWrapper.java
@@ -1,8 +1,6 @@
 package guangyin.internal.types;
 
-import clojure.lang.ISeq;
 import clojure.lang.Keyword;
-import clojure.lang.MapEntry;
 import clojure.lang.IMapEntry;
 import clojure.lang.Associative;
 import clojure.lang.IPersistentMap;
@@ -18,14 +16,6 @@ public class TemporalWrapper extends TemporalAccessorWrapper implements Associat
         this.wrapped = wrapped;
     }
 
-    public ISeq seq() {
-        throw new UnsupportedOperationException("Seq not supported by temporal");
-    }
-
-    public int count() {
-        throw new UnsupportedOperationException("Count not supported by temporal");
-    }
-
     public IPersistentCollection cons(Object o) {
         throw new UnsupportedOperationException("Cons not supported by temporal");
     }
@@ -39,21 +29,11 @@ public class TemporalWrapper extends TemporalAccessorWrapper implements Associat
     }
 
     public boolean containsKey(Object key) {
-        Keyword keyword = (Keyword) key;
-        TemporalField field = (TemporalField) keyword.invoke(this.keymap);
-        if (field == null) {
-            return false;
-        }
-        return wrapped.isSupported(field);
+        return this.valmap().containsKey(key);
     }
 
     public IMapEntry entryAt(Object key) {
-        Keyword keyword = (Keyword) key;
-        TemporalField field = (TemporalField) keyword.invoke(this.keymap);
-        if (field == null || !wrapped.isSupported(field)) {
-            return null;
-        }
-        return new MapEntry(key, wrapped.getLong(field));
+        return this.valmap().entryAt(key);
     }
 
     public Associative assoc(Object key, Object val) {
