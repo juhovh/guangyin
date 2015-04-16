8d879ccd2774a68fa1191179f5dceece1b093961
Juho Vähä-Herttua <juhovh@iki.fi>
2015-04-16 16:07:46 +0300

    Add different Clojure profiles to run tests against.

--- project.clj
+++ project.clj
@@ -11,4 +11,8 @@
   :javac-options ["-target" "1.8" "-source" "1.8"]
   :profiles {:dev {:plugins [[codox "0.8.11"]
                              [lein-cloverage "1.0.2"]]
-                   :codox {:include [guangyin.core guangyin.format guangyin.chrono]}}})
+                   :codox {:include [guangyin.core guangyin.format guangyin.chrono]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta1"]]}})
