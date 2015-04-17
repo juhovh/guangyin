6f7750f82341522b1a6b783a1ab48669fcba159b
Juho Vähä-Herttua <juhovh@iki.fi>
2015-04-17 11:09:00 +0300

    Add documentation and tests for guangyin.chrono namespace.

--- src/clj/guangyin/chrono.clj
+++ src/clj/guangyin/chrono.clj
@@ -5,15 +5,51 @@
                              ThaiBuddhistDate)))
 
 (defn iso-era
+  "Get ISO era.
+   Examples:
+
+     => (iso-era :bce)
+     #<IsoEra BCE>
+     => (iso-era :ce)
+     #<IsoEra CE>"
   [key]
   (wrap (fields/get-field fields/iso-eras key)))
 
 (defn hijrah-era
+  "Get Hijri calendar era.
+   Examples:
+
+     => (hijrah-era :ah)
+     #<HijrahEra AH>"
   [key]
   (wrap (fields/get-field fields/hijrah-eras key)))
 
+(defn hijrah-date?
+  "Returns true if the given value is a Hijri calendar date.
+   Examples:
+
+     => (hijrah-date? (hijrah-date :now))
+     true
+     => (hijrah-date? (minguo-date :now))
+     false"
+  [x]
+  (instance? HijrahDate (unwrap x)))
+
 (defprotocol IHijrahDate
-  (hijrah-date [this] [year month day]))
+  (hijrah-date [this] [year month day]
+   "Coerce to Hijri calendar date.
+    Examples:
+
+      => (hijrah-date :now)
+      #<HijrahDate Hijrah-umalqura AH 1436-06-28>
+      => (hijrah-date (clock))
+      #<HijrahDate Hijrah-umalqura AH 1436-06-28>
+      => (hijrah-date (zone-id \"Asia/Shanghai\"))
+      #<HijrahDate Hijrah-umalqura AH 1436-06-28>
+      => (hijrah-date (local-date :now))
+      #<HijrahDate Hijrah-umalqura AH 1436-06-28>
+      => (hijrah-date 1436 6 28)
+      #<HijrahDate Hijrah-umalqura AH 1436-06-28>"))
 
 (extend-protocol IHijrahDate
   guangyin.internal.types.IWrapper
@@ -31,19 +67,60 @@
   (hijrah-date [year month day] (wrap (HijrahDate/of year month day))))
 
 (defn japanese-era
+  "Get Japanese calendar era.
+   Examples:
+
+     => (japanese-era :heisei)
+     #<JapaneseEra Heisei>
+     => (japanese-era :meiji)
+     #<JapaneseEra Meiji>
+     => (japanese-era :showa)
+     #<JapaneseEra Showa>
+     => (japanese-era :taisho)
+     #<JapaneseEra Taisho>"
   [key]
   (wrap (fields/get-field fields/japanese-eras key)))
 
+(defn japanese-date?
+  "Returns true if the given value is a Japanese calendar date.
+   Examples:
+
+     => (japanese-date? (japanese-date :now))
+     true
+     => (japanese-date? (minguo-date :now))
+     false"
+  [x]
+  (instance? JapaneseDate (unwrap x)))
+
 (defprotocol IJapaneseDate
-  (japanese-date [this] [year month day] [era year month day]))
+  (japanese-date [this] [year month day] [era year month day]
+   "Coerce to Japanese calendar date.
+    Examples:
+
+      => (japanese-date :now)
+      #<JapaneseDate Japanese Heisei 27-04-17>
+      => (japanese-date (clock))
+      #<JapaneseDate Japanese Heisei 27-04-17>
+      => (japanese-date (zone-id \"Asia/Shanghai\"))
+      #<JapaneseDate Japanese Heisei 27-04-17>
+      => (japanese-date (local-date :now))
+      #<JapaneseDate Japanese Heisei 27-04-17>
+      => (japanese-date 2015 4 17)
+      #<JapaneseDate Japanese Heisei 27-04-17>
+      => (japanese-date :showa 27 4 17)
+      #<JapaneseDate Japanese Showa 27-04-17>
+      => (japanese-date (japanese-era :showa) 27 4 17)
+      #<JapaneseDate Japanese Showa 27-04-17>"))
 
 (extend-protocol IJapaneseDate
   guangyin.internal.types.IWrapper
   (japanese-date ([this] (japanese-date @this))
                  ([era year month day] (japanese-date @era year month day)))
   clojure.lang.Keyword
-  (japanese-date [this] (when (= this :now)
-                              (wrap (JapaneseDate/now))))
+  (japanese-date ([this] (when (= this :now)
+                               (wrap (JapaneseDate/now))))
+                 ([era year month day]
+                  (wrap (JapaneseDate/of @(japanese-era era) year month day))))
   java.time.temporal.TemporalAccessor
   (japanese-date [this] (wrap (JapaneseDate/from this)))
   java.time.chrono.JapaneseEra
@@ -57,11 +134,42 @@
   (japanese-date [year month day] (wrap (JapaneseDate/of year month day))))
 
 (defn minguo-era
+  "Get Minguo calendar era.
+   Examples:
+
+     => (minguo-era :before-roc)
+     #<MinguoEra BEFORE_ROC>
+     => (minguo-era :roc)
+     #<MinguoEra ROC>"
   [key]
   (wrap (fields/get-field fields/minguo-eras key)))
 
+(defn minguo-date?
+  "Returns true if the given value is a Minguo calendar date.
+   Examples:
+
+     => (minguo-date? (minguo-date :now))
+     true
+     => (minguo-date? (hijrah-date :now))
+     false"
+  [x]
+  (instance? MinguoDate (unwrap x)))
+
 (defprotocol IMinguoDate
-  (minguo-date [this] [year month day]))
+  (minguo-date [this] [year month day]
+   "Coerce to Minguo calendar date.
+    Examples:
+
+      => (minguo-date :now)
+      #<MinguoDate Minguo ROC 104-04-17>
+      => (minguo-date (clock))
+      #<MinguoDate Minguo ROC 104-04-17>
+      => (minguo-date (zone-id "Asia/Shanghai"))
+      #<MinguoDate Minguo ROC 104-04-17>
+      => (minguo-date (local-date :now))
+      #<MinguoDate Minguo ROC 104-04-17>
+      => (minguo-date 104 4 17)
+      #<MinguoDate Minguo ROC 104-04-17>"))
 
 (extend-protocol IMinguoDate
   guangyin.internal.types.IWrapper
@@ -79,11 +187,42 @@
   (minguo-date [year month day] (wrap (MinguoDate/of year month day))))
 
 (defn thai-buddhist-era
+  "Get Thai buddhist calendar era.
+   Examples:
+
+     => (thai-buddhist-era :before-be)
+     #<ThaiBuddhistEra BEFORE_BE>
+     => (thai-buddhist-era :be)
+     #<ThaiBuddhistEra BE>"
   [key]
   (wrap (fields/get-field fields/thai-buddhist-eras key)))
 
+(defn thai-buddhist-date?
+  "Returns true if the given value is a Thai buddhist calendar date.
+   Examples:
+
+     => (thai-buddhist-date? (thai-buddhist-date :now))
+     true
+     => (thai-buddhist-date? (minguo-date :now))
+     false"
+  [x]
+  (instance? ThaiBuddhistDate (unwrap x)))
+
 (defprotocol IThaiBuddhistDate
-  (thai-buddhist-date [this] [year month day]))
+  (thai-buddhist-date [this] [year month day]
+   "Coerce to Thai buddhist calendar date.
+    Examples:
+
+      => (thai-buddhist-date :now)
+      #<ThaiBuddhistDate ThaiBuddhist BE 2558-04-17>
+      => (thai-buddhist-date (clock))
+      #<ThaiBuddhistDate ThaiBuddhist BE 2558-04-17>
+      => (thai-buddhist-date (zone-id \"Asia/Shanghai\"))
+      #<ThaiBuddhistDate ThaiBuddhist BE 2558-04-17>
+      => (thai-buddhist-date (local-date :now))
+      #<ThaiBuddhistDate ThaiBuddhist BE 2558-04-17>
+      => (thai-buddhist-date 2558 4 17)
+      #<ThaiBuddhistDate ThaiBuddhist BE 2558-04-17>"))
 
 (extend-protocol IThaiBuddhistDate
   guangyin.internal.types.IWrapper
--- test/guangyin/chrono_test.clj
+++ test/guangyin/chrono_test.clj
@@ -1,5 +1,6 @@
 (ns guangyin.chrono-test
   (:require [clojure.test :refer :all]
+            [guangyin.core :refer :all]
             [guangyin.chrono :refer :all]))
 
 (deftest test-iso-era
@@ -9,16 +10,48 @@
 (deftest test-hijrah-era
   (is @(hijrah-era :ah)))
 
+(deftest test-hijrah-date
+  (is (hijrah-date? @(hijrah-date :now)))
+  (is (hijrah-date? @(hijrah-date (clock))))
+  (is (hijrah-date? @(hijrah-date (zone-id "Asia/Shanghai"))))
+  (is (hijrah-date? @(hijrah-date (local-date :now))))
+  (is (hijrah-date? @(hijrah-date 1436 6 28))))
+
 (deftest test-japanese-era
   (is @(japanese-era :heisei))
   (is @(japanese-era :meiji))
   (is @(japanese-era :showa))
   (is @(japanese-era :taisho)))
 
+(deftest test-japanese-date
+  (is (japanese-date? @(japanese-date :now)))
+  (is (japanese-date? @(japanese-date (clock))))
+  (is (japanese-date? @(japanese-date (zone-id "Asia/Shanghai"))))
+  (is (japanese-date? @(japanese-date (local-date :now))))
+  (is (japanese-date? @(japanese-date 2015 4 17)))
+  (is (japanese-date? @(japanese-date :showa 27 4 17)))
+  (is (japanese-date? @(japanese-date (japanese-era :showa) 27 4 17))))
+
 (deftest test-minguo-era
   (is @(minguo-era :before-roc))
   (is @(minguo-era :roc)))
 
+(deftest test-minguo-date
+  (is (minguo-date? @(minguo-date :now)))
+  (is (minguo-date? @(minguo-date (clock))))
+  (is (minguo-date? @(minguo-date (zone-id "Asia/Shanghai"))))
+  (is (minguo-date? @(minguo-date (local-date :now))))
+  (is (minguo-date? @(minguo-date 104 4 17))))
+
 (deftest test-thai-buddhist-era
   (is @(thai-buddhist-era :before-be))
   (is @(thai-buddhist-era :be)))
+
+(deftest test-thai-buddhist-date
+  (is (thai-buddhist-date? @(thai-buddhist-date :now)))
+  (is (thai-buddhist-date? @(thai-buddhist-date (clock))))
+  (is (thai-buddhist-date? @(thai-buddhist-date (zone-id "Asia/Shanghai"))))
+  (is (thai-buddhist-date? @(thai-buddhist-date (local-date :now))))
+  (is (thai-buddhist-date? @(thai-buddhist-date 2558 4 17))))
+
+
